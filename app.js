86fbe1b308aabe0eeeacb449341c92bde75c7855
onlyjackfrost <onlyjackfrost@gmail.com>
2020-02-29 11:38:00 +0800

    mongo init, new Todo model

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const port = 3000;
 const app = express()
+const Todo = require('./model/todo')
+
+mongoose.connect('mongodb://localhost/Todo', { useNewUrlParser: true,useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error',(err)=>{
+    console.log('db error:',err)
+})
+db.once('open',()=>{
+    console.log('db connected')
+    app.listen(port,()=>{
+        console.log('listen on port :',port)
+    })
+})
 
 app.get('/',(req,res)=>{
     console.log('hi')
-})
-
-app.listen(port,()=>{
-    console.log('listen on port :',port)
 })
